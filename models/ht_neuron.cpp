ac2a05189138a381c8a74e137f71e34ac518c572
Keiko Fujii <kfujii2@wisc.edu>
2016-10-13 10:22:25 -0500

    Removed lines for weights x -1

--- models/ht_neuron.cpp
+++ models/ht_neuron.cpp
@@ -760,19 +760,8 @@ nest::ht_neuron::handle( SpikeEvent& e )
   assert( e.get_delay() > 0 );
   assert( e.get_rport() < static_cast< int >( B_.spike_inputs_.size() ) );
 
-  //--- original
-  //B_.spike_inputs_[ e.get_rport() ].add_value(
-  //  e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
-  //  e.get_weight() * e.get_multiplicity() );
-
-  //--- keiko
-  if( e.get_weight() > 0.0 ){
-    B_.spike_inputs_[ e.get_rport() ].add_value(e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
-						e.get_weight() * e.get_multiplicity() );
-  }else{
-    B_.spike_inputs_[ e.get_rport() ].add_value(e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
-						-e.get_weight() * e.get_multiplicity() );
-  }
+  B_.spike_inputs_[ e.get_rport() ].add_value(e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
+					      e.get_weight() * e.get_multiplicity() );
 
   //--- keiko
   B_.sender_gid_[ e.get_rport() ].add_value(e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
